58bade9d0ba0c46a0460d4aabc2408550dcbc022
hackers365 <hackers365@gmail.com>
2013-07-21 19:11:10 +0800

    add file

--- include/my.js
+++ include/my.js
@@ -5,18 +5,22 @@ ws.on('open', open);
 
 ws.open('ws://localhost:10001', 'base64');
 
+$(document)
+.on('click', '#send', on_write);
 console.log(ws);
 
 function on_read() {
     var len = ws.rQlen();
     if (len > 0) {
         var str = ws.rQshiftStr(len);
-        $('#view').append('\n' + str);
+        $('#view').val($('#view').val() + '\n' + str);
     }
 }
 
 function on_write() {
-
+    var str = $('#send_text').val() + "\n";
+    console.log(str);
+    ws.send(str);
 }
 
 function open() {
