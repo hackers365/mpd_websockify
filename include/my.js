0d1cab5a89c1937a05e4e4dbb4556abb66a0975e
hackers365 <hackers365@gmail.com>
2013-07-21 21:39:24 +0800

    可以发命令了

--- include/my.js
+++ include/my.js
@@ -1,13 +1,19 @@
-var ws = new Websock();
-ws.on('message', on_read);
-ws.on('close', close);
-ws.on('open', open);
-
-ws.open('ws://localhost:10001', 'base64');
-
+var fail_count = 0;
+var ws;
 $(document)
-.on('click', '#send', on_write);
-console.log(ws);
+.on('click', '#send', on_write)
+.on('keypress', '#send_text', key_write);
+
+connect();
+function connect() {
+    ws = new Websock();
+    ws.on('message', on_read);
+    ws.on('close', close);
+    ws.on('open', open);
+
+    var r = ws.open('ws://localhost:10001', 'base64');
+    console.log(r);
+}
 
 function on_read() {
     var len = ws.rQlen();
@@ -17,16 +23,32 @@ function on_read() {
     }
 }
 
+function key_write(e) {
+    if (typeof(e) == 'object' && e.keyCode == 13) {
+        on_write();
+    }
+}
 function on_write() {
     var str = $('#send_text').val() + "\n";
-    console.log(str);
-    ws.send(str);
+    ws.send_string(str);
+    $('#send_text').val('');
 }
 
 function open() {
 
 }
 
+function fail_connect() {
+    $('#info').text('try to connect count ' + fail_count);
+    return function() {
+        connect();
+    }
+}
 function close() {
-
+    if (fail_count > 5) {
+        $('#info').text('fail connect');
+        return;
+    }
+    setTimeout(connect, 5000 * fail_count);
+    fail_count++;
 }
